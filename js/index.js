ed24a9961a83d083a22676baccd5525c32886830
collinskoech11 <collinskoechck34@gmail.com>
2020-07-21 17:37:19 +0300

    Added optional topbar for smaller screen width

--- js/index.js
+++ js/index.js
@@ -2,9 +2,11 @@ document.getElementById('btn').addEventListener('click',
 function() {
 	document.querySelector('.desc').style.display = 'block';
 	document.querySelector('#btn').style.display = 'none';
+	document.querySelector('.header-opt').style.display = 'none';
 });
 document.querySelector('.close').addEventListener('click',
 	function() {
 		document.querySelector('.desc').style.display = 'none';
 		document.querySelector('#btn').style.display = 'block';
+		document.querySelector('.header-opt').style.display = 'inline-block';
 	})
