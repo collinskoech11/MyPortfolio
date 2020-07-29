4c7d5ca0456e4ab6d9e2fb75e62d027381f0ea23
collinskoech11 <collinskoechck34@gmail.com>
2020-07-29 14:00:45 +0300

    Hosted by firebase hosting

---
+++ public/js/index.js
@@ -0,0 +1,36 @@
+document.getElementById('btn').addEventListener('click',
+function() {
+	document.querySelector('.desc').style.display = 'block';
+	document.querySelector('#btn').style.display = 'none';
+	document.querySelector('.header-opt').style.display = 'none';
+});
+document.querySelector('.close').addEventListener('click',
+function() {
+	document.querySelector('.desc').style.display = 'none';
+	document.querySelector('#btn').style.display = 'block';
+	document.querySelector('.header-opt').style.display = 'inline-block';
+});
+document.getElementById('dep').addEventListener('click',
+	function() {
+		document.querySelector('.content-wrapper').style.display = 'none';
+		document.querySelector('.os-wrapper').style.display = 'none';
+		document.querySelector('.dep-wrapper').style.display = 'block';
+	});
+document.getElementById('web').addEventListener('click',
+	function() {
+		document.querySelector('.content-wrapper').style.display = 'block';
+		document.querySelector('.os-wrapper').style.display = 'none';
+		document.querySelector('.dep-wrapper').style.display = 'none';
+	});
+document.getElementById('os').addEventListener('click',
+	function() {
+		document.querySelector('.content-wrapper').style.display = 'none';
+		document.querySelector('.os-wrapper').style.display = 'block';
+		document.querySelector('.dep-wrapper').style.display = 'none;'
+	});
+window.addEventListener("scroll", 
+	function() {
+		var header = document.querySelector("#myHeader");
+		myHeader.classList.toggle("sticky", window.scrollY > 100);
+		document.querySelector('.header-opt').style.background = 'lightgray';
+	});
