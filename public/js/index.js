31713d4a1cb6df6171606d80f7c754c7bb59ad49
Collins Koech <collinskoechck34@gmail.com>
2020-09-02 16:59:14 +0300

    update

--- public/js/index.js
+++ public/js/index.js
@@ -16,7 +16,8 @@ function() {
 /**document.getElementById('.closeo').addEventListener('click',
 	function(){
 	document.querySelector('.tweetPop').style.display = 'none';
-	});**/
+	});
+	**/
 document.getElementById('dep').addEventListener('click',
 	function() {
 		document.querySelector('.content-wrapper').style.display = 'none';
