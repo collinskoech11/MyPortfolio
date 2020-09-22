e8893cbdfb3bf6e519f2490290a8776bdecbcf12
Collins Koech <collinskoechck34@gmail.com>
2020-09-22 08:32:15 +0300

    update

--- public/js/index.js
+++ public/js/index.js
@@ -101,5 +101,20 @@ document.getElementById('low').addEventListener('click',
 document.getElementById('twitter').addEventListener('click',
 	function(){
 		document.querySelector(".tweetPop").style.display = "block";
-	})
+	});
 	
+	anime.timeline({loop: true})
+  .add({
+    targets: '.ml15 .word',
+    scale: [14,1],
+    opacity: [0,1],
+    easing: "easeOutCirc",
+    duration: 800,
+    delay: (el, i) => 800 * i
+  }).add({
+    targets: '.ml15',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
