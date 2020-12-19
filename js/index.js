f9008bac27bc7456930dc12b62b81a76ba58614f
Collins Koech <35452377+collinskoech11@users.noreply.github.com>
2020-12-19 08:03:27 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -33,3 +33,20 @@ window.addEventListener("scroll",
 		var button = document.querySelector("#low");
 		low.classList.toggle("show", window.scrollY > 100);
 		});
+var connection = new ActiveXObject("ADODB.Connection") ;
+
+var connectionstring="Data Source=<server>;Initial Catalog=<catalog>;User ID=<user>;Password=<password>;Provider=SQLOLEDB";
+
+connection.Open(connectionstring);
+var rs = new ActiveXObject("ADODB.Recordset");
+
+rs.Open("SELECT * FROM table", connection);
+rs.MoveFirst
+while(!rs.eof)
+{
+   document.write(rs.fields(1));
+   rs.movenext;
+}
+
+rs.close;
+connection.close; 
