155f49437df1e11f1589fa9258679e9b24e81934
collinskoech11 <collinskoechck34@gmail.com>
2021-02-17 14:52:39 +0300

    alignment update

--- js/hello.java
+++
@@ -1,8 +0,0 @@
-import java.lang.Math;
-public class hello{
-    public static void main (String [] args){
-        float y;
-        y = 12 + Math.ceil(Math.random()*37)  % 7;
-        System.out.println(y+"value");
-    }
-}
